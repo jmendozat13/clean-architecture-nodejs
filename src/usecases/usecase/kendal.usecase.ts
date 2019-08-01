a50703bee56d335a4175f82b0fdd89c25c889d23
Maria Franshesca Torres Zegarra <mtorresz@everis.com>
2019-08-01 10:12:17 -0500

    Update metod

--- src/usecases/usecase/kendal.usecase.ts
+++ src/usecases/usecase/kendal.usecase.ts
@@ -2,12 +2,12 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { IKendal } from '../../entities/interfaces/kendal.interfaces';
-import { CreateKendalDto } from '../../entities/dto/kendal.dto';
+import { ClassKendalDto } from '../../entities/dto/kendal.dto';
 
 @Injectable()
 export class KendalUseCase {
     constructor(@InjectModel('Kendal') private readonly kendalModel: Model<IKendal>) { }
-    async create(createKendalDto: CreateKendalDto): Promise<IKendal> {
+    async create(createKendalDto: ClassKendalDto): Promise<IKendal> {
         const createKendal = new this.kendalModel(createKendalDto);
         return await createKendal.save();
     }
@@ -19,6 +19,11 @@ export class KendalUseCase {
     async deleteById(id: string): Promise<string> {
         await this.kendalModel.deleteOne({_id: id});
         return 'Eliminado correctamente';
+    }
+
+    async update(id: string, updateKendalDto: ClassKendalDto): Promise<string> {
+        await this.kendalModel.findByIdAndUpdate(id, updateKendalDto);
+        return "Editado correctamente";
     }
 
 }
