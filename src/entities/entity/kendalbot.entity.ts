4f7c40713df123279462bfafd459090da47dd184
Jesus Valentin Mendoza Torres <afoxplus@gmail.com>
2019-08-02 23:09:38 -0500

    add first version chatbot for kendalbot

--- src/entities/entity/kendalbot.entity.ts
+++ src/entities/entity/kendalbot.entity.ts
@@ -2,12 +2,18 @@ import { ApiModelProperty } from '@nestjs/swagger';
 export class KendalBotResponse {
     @ApiModelProperty()
     outputmessage: string;
-    @ApiModelProperty()
-    options: {
-        title: string;
-        query: string;
-    };
+    options: KendalBotOption[];
     constructor(message: string) {
         this.outputmessage = message;
+        this.options = Array<KendalBotOption>();
+    }
+}
+// tslint:disable-next-line:max-classes-per-file
+export class KendalBotOption {
+    title: string;
+    query: string;
+    constructor(title: string, query: string) {
+        this.title = title;
+        this.query = query;
     }
 }
--- src/usecases/usecase/kendalbot.usecase.ts
+++ src/usecases/usecase/kendalbot.usecase.ts
@@ -2,7 +2,7 @@ import { Model } from 'mongoose';
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { IKendal } from '../../entities/interfaces/kendal.interfaces';
-import { KendalBotResponse } from '../../entities/entity/kendalbot.entity';
+import { KendalBotResponse, KendalBotOption } from '../../entities/entity/kendalbot.entity';
 import { KendalBotDto } from '../../entities/dto/kendalbot.dto';
 
 @Injectable()
@@ -10,9 +10,17 @@ export class KendalBotUseCase {
     constructor(@InjectModel('Kendal') private readonly kendalModel: Model<IKendal>) { }
 
     async chatBot(kendalBotDto: KendalBotDto): Promise<KendalBotResponse> {
-        const kendal = await this.kendalModel.find({ input: kendalBotDto.inputmessage.trimLeft().trimRight() });
-        const messagedefault = 'Hola soy Kendal, el asistente virtual de la UNT. ' +
-            'Mis creadores estan trabajando en mi para ser mas inteligente, dentro de poco te ofrecere la ayuda que necesitas.';
-        return await new KendalBotResponse(messagedefault);
+        const kendalbasic = await this.kendalModel
+            .findOne({ input: kendalBotDto.inputmessage })
+            .lean();
+        if (kendalbasic) {
+            const kendalresponse = new KendalBotResponse(kendalbasic.output);
+            const options = await this.kendalModel.find({ parentid: kendalbasic._id });
+            options.forEach(item => {
+                kendalresponse.options.push(new KendalBotOption(item.title, item.input));
+            });
+            return kendalresponse;
+        }
+        return await new KendalBotResponse('No entiendo');
     }
 }
