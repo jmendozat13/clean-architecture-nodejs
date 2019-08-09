f24eba27d7a2d44c28903f589853e3aff223cd85
Jesus Valentin Mendoza Torres <afoxplus@gmail.com>
2019-08-08 23:23:13 -0500

    deployment user

--- src/swagger.ts
+++ src/swagger.ts
@@ -3,11 +3,11 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 export function setupSwagger(app: INestApplication) {
     const options = new DocumentBuilder()
-        .setTitle('Kendal API')
-        .setDescription('Kendal backend')
+        .setTitle('Clean Architecture NodeJS')
+        .setDescription('The clean architecture for application backend')
         .setVersion('1.0')
-        .setContactEmail('franshesca.torres.zegarra@gmail.com')
-        .setLicense('MIT', 'https://github.com/MariaFranshesca/clean-architecture-nodejs/blob/devKendal/LICENSE')
+        .setContactEmail('jmendozat13@gmail.com')
+        .setLicense('MIT', 'https://github.com/jmendozat13/clean-architecture-nodejs/blob/master/LICENSE')
         .setSchemes('http', 'https')
         //.addBearerAuth()
         .setBasePath('api')
